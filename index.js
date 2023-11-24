f5bdcde9ba079f0b9a3ac09b60ff9867b3a121a2
SabbirAhamedMaruf <sabbirahamedmaruf02@gmail.com>
2023-11-24 22:26:41 +0600

    added server configuration

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Server config
+app.get("/",async(req,res)=>{
+    res.send("Server is runing!")
+})
+
+// handling error request middleware
+app.all("*",(req,res,next)=>{
+    const error = new Error("Bad request",req.url);
+    error.status = 404;
+    next()
+})
+app.use((err,req,res,next)=>{
+    res.status(err.status || 500).json({message:err.message})
+})
+
+const main = async()=>{
+    // connection request for database
+    app.listen(port,()=>{
+        console.log("Log: Server is currently running on =",port);
+    })
+}
+main();
