0b60933ddd5d256d26c9912dc72e4bc900ce3206
SabbirAhamedMaruf <sabbirahamedmaruf02@gmail.com>
2023-11-25 18:00:36 +0600

    modified upazilas post routes

--- src/api/location/upazilasController.js
+++ src/api/location/upazilasController.js
@@ -1,19 +1,23 @@
 const upazilasModel = require("../../model/location/upazilasModel");
+const districtsModel = require("../../model/location/districtsModel");
 
 const upazilasController = async (req, res) => {
   try {
     const userDistricts = req.query.userDistricts;
-    const districtId = parseInt(userDistricts);
-    const result = await upazilasModel.find();
-    const upazilas = result.filter(i=>i.district_id === districtId);
-    res.status(201).json({
-      success: true,
-      data: upazilas,
+    districtsModel.findOne({ name: userDistricts }).then((result) => {
+      upazilasModel
+        .find({ district_id: result.district_id })
+        .then((upazilaResult) => {
+          res.status(201).json({
+            success: true,
+            data: upazilaResult,
+          });
+        });
     });
   } catch (error) {
     res.status(500).json({
       success: false,
-      message: "Error getting upazilas data on database!",
+      message: "Error getting district data on database!",
     });
   }
 };
--- src/schema/user/user.js
+++ src/schema/user/user.js
@@ -11,10 +11,6 @@ const userSchema = new Schema({
     type: String,
     required: true,
   },
-  password: {
-    type: String,
-    required: true,
-  },
   photo: {
     type: String,
     required: true,
